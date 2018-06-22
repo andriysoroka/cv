46e18e3d57994a05e70a2fdf97c74278c783019d
andriysoroka <dree0103@gmail.com>
2018-06-22 20:06:13 +0300

    done

--- src/App.js
+++ src/App.js
@@ -3,95 +3,140 @@ import foto from './n.jpg';
 import './App.css';
 
 const content = {
-  en: [
-    {
+  en: {
+    fullName: "Andrii Soroka",
+    lenguage: {
       toUkr: "ukr",
       toEng: "en"
     },
-    {
-      full_name: 'Andrii Soroka',
-      phone: ['phone', '063768542'],
-      mail: [' mail', 'dree0103@gmail.com'],
-      age: ['age', 23],
-      nationality: ['nationality', 'Ukrainian'],
-      city: ['city', 'Lviv'],
-    },
-    {
-      title: "GOAL",
-      bold_text: "",
-      text: "I am looking for a job as Junior front-end developer  in a company of curious and active people.",
-      list: []
-    },{
-      title: "SKILLS",
-      bold_text: "",
-      text: "",
-      list: ['HTML', 'CSS', 'JavaScript']
-    },{
-      title: "WORK EXPERIENCE",
-      bold_text: "February 2018 – April 2018",
-      text: "Administrator, “ESCAPE QUEST” ",
-      list: []
-    },{
-      title: "EDUCATION",
-      bold_text: "Lviv Polytechnic National University ",
-      text: "Master Degree. Field of study ‘Publishing information technologies’.",
-      list: []
-    },{
-      title: "COURSES",
-      bold_text: "",
-      text: "Successfully finished JavaScript course at ‘OKTENWEB UNIVERSITY’ and also passed such online courses as:",
-      list: ['Codecademy', 'W3Schools', 'HTML Academy']
-    },{
-      title: "INTERESTS",
-      bold_text: "",
-      text: "Triathlon (swimming, biking and running), computer games, traveling, hiking in the mountains.",
-      list: []
-    }
-  ],
-  ukr: [
-    {
+    mains: [
+      {
+        title: "phone",
+        value: "063768542"
+      },
+      {
+        title: "mail", 
+        value: "dree0103@gmail.com"
+      },
+      {
+        title: "age",
+        value:  23
+      },
+      {
+        title: "nationality",
+        value: "Ukrainian"
+      },
+      {
+        title: "city", 
+        value: "Lviv"
+      }
+    ],
+    deteils: [
+      {
+        title: "GOAL",
+        boldText: "",
+        text: "I am looking for a job as Junior front-end developer  in a company of curious and active people.",
+        list: []
+      },
+      {
+        title: "SKILLS",
+        boldText: "",
+        text: "",
+        list: ["HTML", "CSS", "JavaScript"]
+      },
+      {
+        title: "WORK EXPERIENCE",
+        boldText: "February 2018 – April 2018",
+        text: "Administrator, “ESCAPE QUEST” ",
+        list: []
+      },
+      {
+        title: "EDUCATION",
+        boldText: "Lviv Polytechnic National University ",
+        text: "Master Degree. Field of study ‘Publishing information technologies’.",
+        list: []
+      },
+      {
+        title: "COURSES",
+        boldText: "",
+        text: "Successfully finished JavaScript course at ‘OKTENWEB UNIVERSITY’ and also passed such online courses as:",
+        list: ["Codecademy", "W3Schools", "HTML Academy"]
+      },
+      {
+        title: "INTERESTS",
+        boldText: "",
+        text: "Triathlon (swimming, biking and running), computer games, traveling, hiking in the mountains.",
+        list: []
+      }
+    ]
+  },
+  ukr: {
+    fullName: "Андрій Сорока",
+    lenguage: {
       toUkr: "укр",
       toEng: "англ",
     },
-    {
-      full_name: 'Андрій Сорока',
-      phone: ['телефон', '063768542'],
-      mail: ['пошта', 'dree0103@gmail.com'],
-      age: ['вік', 23],
-      nationality: ['національність', 'українець'],
-      city: ['місто', 'Львів'],
-    },{
-      title: "ЦІЛЬ",
-      bold_text: "",
-      text: "Я шукаю роботу як молодшого фронт-розробника в компанії цікавих та активних людей.",
-      list: []
-      }, {
-      title: "НАВИКИ",
-      bold_text: "",
-      text: "",
-      list: ['HTML', 'CSS', 'JavaScript']
-    }, {
-      title: "ДОСВІД РОБОТИ",
-      bold_text: "Лютий 2018 - квітень 2018 року ",
-      text: "Адміністратор, 'ESCAPE QUEST' ",
-      list: []
-    }, {
-      title: "ОСВІТА",
-      bold_text: "Національний університет 'Львівська політехніка'",
-      text: "Магістр, Сфера навчання 'Видавнича діяльність інформаційних технологій'.",
-      list: []
-    }, {
-      title: "КУРСИ",
-      bold_text: "",
-      text: "Успішно закінчив курс JavaScript на 'ОКТЕНВЕБ УНІВЕРСИТЕТ', а також пройшов такі онлайн курси:",
-      list: ['Codecademy', 'W3Schools', 'HTML Academy']
-    }, {
-      title: "ІНТЕРЕС",
-      bold_text: "",
-      text: "Триатлон (плавання, їзда на велосипеді та біг), комп'ютерні ігри, подорожі, походи в гори.",
-      list: []
-    }
-  ]
+    mains: [
+      {
+        title: "телефон",
+        value: "063768542"
+      },
+      {
+        title: "пошта", 
+        value: "dree0103@gmail.com"
+      },
+      {
+        title: "вік",
+        value:  23
+      },
+      {
+        title: "національність",
+        value: "українець"
+      },
+      {
+        title: "місто", 
+        value: "Львів"
+      }
+    ],
+    deteils: [
+      {
+        title: "ЦІЛЬ",
+        boldText: "",
+        text: "Я шукаю роботу як молодшого фронт-розробника в компанії цікавих та активних людей.",
+        list: []
+      }, 
+      {
+        title: "НАВИКИ",
+        boldText: "",
+        text: "",
+        list: ["HTML", "CSS", "JavaScript"]
+      }, 
+      {
+        title: "ДОСВІД РОБОТИ",
+        boldText: "Лютий 2018 - квітень 2018 року ",
+        text: "Адміністратор, 'ESCAPE QUEST' ",
+        list: []
+      }, 
+      {
+        title: "ОСВІТА",
+        boldText: "Національний університет 'Львівська політехніка'",
+        text: "Магістр, Сфера навчання 'Видавнича діяльність інформаційних технологій'.",
+        list: []
+      }, 
+      {
+        title: "КУРСИ",
+        boldText: "",
+        text: "Успішно закінчив курс JavaScript на 'OKTENWEB UNIVERSITY', а також пройшов такі онлайн курси:",
+        list: ["Codecademy", "W3Schools", "HTML Academy"]
+      }, 
+      {
+        title: "ІНТЕРЕС",
+        boldText: "",
+        text: "Триатлон (плавання, їзда на велосипеді та біг), комп'ютерні ігри, подорожі, походи в гори.",
+        list: []
+      }
+    ]
+  }
 };
 
 const LanguageContext = createContext();
@@ -100,7 +145,7 @@ class Header extends React.Component {
   render () {
     return (
       <header className="main-header">
-        <MainInfo mainInfo={this.props.mainInfo} />
+        <MainInfo name={this.props.name} mainInfo={this.props.mainInfo} />
         <Foto foto={this.props.foto} />
       </header>
     )
@@ -111,17 +156,18 @@ class MainInfo extends React.Component {
   render () {
     return (
       <div className="main-info">
-        {Object.keys(this.props.mainInfo).map(item => <InfoItem key={item} item={item} info={this.props.mainInfo[item]} />)}
+        <h2>{this.props.name}</h2>
+        {this.props.mainInfo.map(item => <MainsItem key={item} item={item} info={this.props.mainInfo[item]} />)}
       </div>
     )
   }
 }
 
-class InfoItem extends React.Component {
+class MainsItem extends React.Component {
   render () {
     return (
-    <div>{this.props.item !== 'full_name' ? (<span>{this.props.info[0]}: </span>): ('')}
-    {this.props.item !== 'full_name' ? (<span>{this.props.info[1]}</span>) : <h2>{this.props.info}</h2>}
+    <div>
+      {this.props.item.title}: {this.props.item.value}
     </div>
     )
   }
@@ -137,22 +183,6 @@ class Foto extends React.Component {
   }
 }
 
-class Item extends React.Component {
-  render () {
-    return (
-      <div className="item">
-        <div>{this.props.item.title}</div>
-        <div className="item-info">
-          <div>{this.props.item.bold_text}</div>
-          <div>{this.props.item.text}</div>
-          <ul>{ this.props.item.list.length > 0 ?
-          this.props.item.list.map((item, index) => <Skill key={index} item={item}/>)
-          : ("")}</ul>
-        </div>
-      </div>
-    )
-  }
-}
 
 class MainArticle extends React.Component {
   render () {
@@ -164,7 +194,22 @@ class MainArticle extends React.Component {
   }
 }
 
-
+class Item extends React.Component {
+  render () {
+    return (
+      <div className="item">
+        <div className="bold">{this.props.item.title}</div>
+        <div className="item-info">
+          <div className="bold">{this.props.item.boldText}</div>
+          <div>{this.props.item.text}</div>
+          <ul>{this.props.item.list.length > 0 ?
+          this.props.item.list.map((item, index) => <Skill key={index} item={item}/>)
+          : ("")}</ul>
+        </div>
+      </div>
+    )
+  }
+}
 
 class Skill extends React.Component {
 	render() {
@@ -173,8 +218,6 @@ class Skill extends React.Component {
 		)
 	}
 }
-
-
 
 class App extends Component {
   constructor() {
@@ -193,14 +236,14 @@ class App extends Component {
               <div>
                 <div>
                   <button onClick={() => this.setState({ language: "en" })}>
-                    {content[0].toEng}
+                    {content.lenguage.toEng}
                   </button>
                   <button onClick={() => this.setState({ language: "ukr" })}>
-                    {content[0].toUkr}
+                    {content.lenguage.toUkr}
                   </button>
                 </div>
-                <Header mainInfo={content[1]} foto={foto} />
-                <MainArticle items={content.slice(2)} />
+                <Header name={content.fullName} mainInfo={content.mains} foto={foto} />
+                <MainArticle items={content.deteils} />
               </div>
             );
           }}
